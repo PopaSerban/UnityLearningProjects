9c5c99a170994d44b4e94601c42d7cec229037a1
PopaSerban <serban.popa@outlook.com>
2020-06-23 23:32:22 +0300

    Small 2d Character Controller
    Tested and implemented a 2d character controller with climbing system and a parallax system for the boackground.

---
+++ NoneTale/Assets/NoneTale/_Scripts/Background Effects/Parallaxing.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour {
+
+    [SerializeField]
+    private Transform[] backgrounds;
+    private float[] parallaxScales;        //The proportion of the camera's movement to move the backgrounds by
+    public float smoothing = 1f;          // How smooth the parallax is going to be, Make sure to set this above 0.
+
+    private Transform cam;               //Reference to the main cameras transform.
+    private Vector3 previousCamPosition; //The position of the camera in the previous frame
+
+
+
+    //This is called before Start(). good for refferences
+     void Awake()
+    {
+        //set up camera for refference
+        cam = Camera.main.transform;
+        
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        //The previous frame had the urrent frame's camera position
+        previousCamPosition = cam.position;
+
+        //Assigning coresponding prallaxScales;
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //foreach background
+        for(int i = 0; i < backgrounds.Length; i++)
+        {
+            //the parallax is the opposite of the camera movement because of the previous frame multiplied by the scale;
+            float parallax = (previousCamPosition.x - cam.position.x) * parallaxScales[i];
+
+            //Set a  target x position whic is the current possition plus the parallax
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            //create a target position is the background's current position with its target x position
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            //fade between current position and the target position using lerp
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        //set the previousCamPosition to the camera's position at the end of the frame;
+        previousCamPosition = cam.position;
+		
+	}
+}
---
+++ NoneTale/Assets/NoneTale/_Scripts/Usable/ClimbTrees.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbTrees : MonoBehaviour,IUseable {
+  
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Use()
+    {
+        //Debug.Log("Now you can climb you piece of shit");
+        if (Player.Instance._onTree)
+        {
+            UseTree(false,1,0);
+        }
+        else
+        {
+            UseTree(true,0,1);
+
+        }
+    }
+
+    private void UseTree(bool onLadder,int gravity, int layerWeight)
+    {
+        Player.Instance._onTree = onLadder;
+        Player.Instance._rigidbody2D.gravityScale = gravity;
+        Player.Instance._animator.SetLayerWeight(2, layerWeight);
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.tag == "Player")
+        {
+            UseTree(false, 1,0);
+        }
+    }
+}
